a695d91aac1cfb0a4f02b7ea4de0f0e039cb2c84
Jakubcho <jakub.chola92@gmail.com>
2023-07-24 14:51:07 +0200

    Add blog

---
+++ pages/contact.js
@@ -0,0 +1,21 @@
+const Contact = () => {
+    return (
+        <>
+        <h1>
+            Contact
+        </h1>
+        <p>
+            55 serek
+        </p>
+        <p>4x24=96 </p>
+        Zapotrzebowanie 110g
+
+        <h2>kcal 2908 kcal</h2>
+        <p>serek 500kcal</p>
+        <p>białko 113x4=452</p>
+        </>
+
+    )
+}
+
+export default Contact;
